51cd81f637730328a5bd39c6ede0611cef8f2d76
pedrovinhas <pstvideos@gmail.com>
2022-10-04 01:40:48 -0300

    add: js - dom

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Formulário
+const activitiesContent = document.querySelector(".form-input__activities");
+const addTaskButton = document.querySelector(".planner-bar__add");
+const cardItem = document.querySelector(".card-tasks__item");
+const selectedDay = document.getElementById("option-day");
+const plannerDaysOfWeek = document.querySelectorAll("[data-day]");
+const dayTime = document.getElementById("option-time");
+const excludeAllTaskBtn = document.querySelector(".planner-bar__exclude");
+
+const planner = document.querySelector(".planner-calendar__table");
+
+const cardTime = document.querySelectorAll(".hours__card");
+
